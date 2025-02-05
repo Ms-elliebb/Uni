a494c5dc70913eb3ea12cf323ef95fd94aa60d27
ellie <elifbeyzabelhan1@gmail.com>
2025-02-05 23:14:32 +0300

    feat: Add language translations and localization - Add translation JSON files - Create AppLocalizations class - Configure asset bundles for translations - Implement translation helper methods

---
+++ lib/languages/app_localizations.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppLocalizations {
+  final Locale locale;
+  late Map<String, dynamic> _localizedStrings;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations)!;
+  }
+
+  Future<bool> load() async {
+    String jsonString = await rootBundle.loadString(
+        'lib/languages/translations/${locale.languageCode}.json');
+    _localizedStrings = json.decode(jsonString);
+    return true;
+  }
+
+  String translate(String key, [List<dynamic>? args]) {
+    String translation = _localizedStrings[key] ?? key;
+    if (args != null) {
+      args.forEach((arg) {
+        translation = translation.replaceFirst('%s', arg.toString());
+      });
+    }
+    return translation;
+  }
+} 
