783abbff0f7c98a32b4be478836307ac59e56129
ellie <elifbeyzabelhan1@gmail.com>
2025-02-05 23:35:04 +0300

    fix: Update localization tests - Add TestWidgetsFlutterBinding initialization - Implement mock asset loading for tests - Add test JSON data

--- test/localization_test.dart
+++ test/localization_test.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
 import 'package:uni/languages/app_localizations.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('AppLocalizations Tests', () {
     late AppLocalizations localizations;
 
     setUp(() async {
+      // Test için sahte JSON dosyası içeriği oluştur
+      const String testJson = '''
+      {
+        "welcome": "Welcome",
+        "settings": "Settings",
+        "uninstallCount": "%d apps uninstalled",
+        "failedUninstalls": "Failed to uninstall: %s"
+      }
+      ''';
+
+      // Sahte asset yükleme mekanizması
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMessageHandler('flutter/assets', (ByteData? message) async {
+        return ByteData.sublistView(Uint8List.fromList(testJson.codeUnits));
+      });
+
       localizations = AppLocalizations(const Locale('en'));
       await localizations.load();
     });
